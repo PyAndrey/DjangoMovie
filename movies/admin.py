bc9db5a1cd4c30651ab3c2d7d855b8e5260a9442
PyAndrey <yuzvuk.andrey02@mai.ru>
2020-01-12 22:42:18 +0300

    Добавлено 2 фильма в БД

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Actor, Category, Genre, Movie, MoviesShots, Rating,
+                     RatingStar, Reviews)
+
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MoviesShots)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Reviews)
